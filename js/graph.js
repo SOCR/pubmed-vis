98e65fcf7262dbbf90ba5a38db804d5d5362e8f5
tan-patrick <PatrickTioTan@gmail.com>
2013-07-20 17:41:43 -0700

    single search bar, basic API code

--- js/graph.js
+++ js/graph.js
@@ -1,119 +1,159 @@
-var w = 960,
-    h = 500,
-    root;
+//api key:f6bc08acc6c7dbd33c60f04ac9d55f38
+(function(){
+  $(document).ready(function(){
+    
+    $("#searchButton").click(function() {
+      // var search_term = "Venter JC[Auth]";
+      var search_term = $("#inputSearch").val();
+        args = {'apikey' : '191d24f81e61c107bca103f7d6a9ca10',
+                'db'     : 'pubmed',
+                'term'   : search_term,
+                'retmax' : 500,          // maximum number of results from Esearch
+                'max'    : 25,          // maximum number of results passed to Esummary
+                'start'  : 0};
+        $.getJSON('http://entrezajax.appspot.com/esearch+esummary?callback=?', args, function(data) {
+          if(data.entrezajax.error == true) {
+          $("#result").html('<p>' + 'Sorry - EntrezAjax failed with error ' + data.entrezajax.error_message + '</p>');
+          return;
+          }
+          $('#result').html(data.entrezajax.count + ' results found<br/>');
+          $.each(data.result, function(i, item) {
+            var author_list = '';
+            for(var i = 0; i < item.AuthorList.length; i ++) {
+              if(i != 0) {
+                author_list += ', ';
+              }
+              author_list += item.AuthorList[i];
+            }
+            var html = '<p><a href=\'http://www.ncbi.nlm.nih.gov/pubmed/' + item.ArticleIds.pubmed + '\'>' + item.Title + '</a><br/>' + author_list + '<br/>' + item.FullJournalName + ' ' + item.PubDate + '</p>';
+            $("<div/>").html(html).appendTo('#result');
+          });
+        });
+      });
 
-var force = d3.layout.force()
-    .linkDistance(80)
-    .charge(-120)
-    .gravity(.05)
-    .size([w, h]);
+    function createGraph()
+    {
 
-var vis = d3.select("#chart").append("svg:svg")
-    .attr("width", w)
-    .attr("height", h);
+    }
 
-d3.json("graph.json", function(json) {
-  root = json;
-  update();
-});
+    var w = 960,
+        h = 500,
+        root;
 
-function update() {
-  var nodes = flatten(root),
-      links = d3.layout.tree().links(nodes);
+    var force = d3.layout.force()
+        .linkDistance(80)
+        .charge(-120)
+        .gravity(.05)
+        .size([w, h]);
 
-  // Restart the force layout.
-  force
-      .nodes(nodes)
-      .links(links)
-      .start();
+    var vis = d3.select("#chart").append("svg:svg")
+        .attr("width", w)
+        .attr("height", h);
 
-  // Update the links…
-  var link = vis.selectAll("line.link")
-      .data(links, function(d) { return d.target.id; });
+    d3.json("graph.json", function(json) {
+      root = json;
+      update();
+    });
 
-  // Enter any new links.
-  link.enter().insert("svg:line", ".node")
-      .attr("class", "link")
-      .attr("x1", function(d) { return d.source.x; })
-      .attr("y1", function(d) { return d.source.y; })
-      .attr("x2", function(d) { return d.target.x; })
-      .attr("y2", function(d) { return d.target.y; });
+    function update() {
+      var nodes = flatten(root),
+          links = d3.layout.tree().links(nodes);
 
-  // Exit any old links.
-  link.exit().remove();
+      // Restart the force layout.
+      force
+          .nodes(nodes)
+          .links(links)
+          .start();
 
-  // Update the nodes…
-  var node = vis.selectAll("g.node")
-      .data(nodes, function(d) { return d.id; })
+      // Update the links…
+      var link = vis.selectAll("line.link")
+          .data(links, function(d) { return d.target.id; });
 
-  node.select("circle")
-      .style("fill", color);
+      // Enter any new links.
+      link.enter().insert("svg:line", ".node")
+          .attr("class", "link")
+          .attr("x1", function(d) { return d.source.x; })
+          .attr("y1", function(d) { return d.source.y; })
+          .attr("x2", function(d) { return d.target.x; })
+          .attr("y2", function(d) { return d.target.y; });
 
-  // Enter any new nodes.
-  var nodeEnter = node.enter().append("svg:g")
-      .attr("class", "node")
-      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
-      .on("click", click)
-      .on("dblclick", dblclick)
-      .call(force.drag);
+      // Exit any old links.
+      link.exit().remove();
 
-  nodeEnter.append("svg:circle")
-      .attr("r", function(d) { return Math.sqrt(d.size) / 10 || 4.5; })
-      .style("fill", color);
+      // Update the nodes…
+      var node = vis.selectAll("g.node")
+          .data(nodes, function(d) { return d.id; })
 
-  nodeEnter.append("svg:text")
-      .attr("text-anchor", "middle")
-      .attr("dy", ".35em")
-      .text(function(d) { return d.name; });
+      node.select("circle")
+          .style("fill", color);
 
-  // Exit any old nodes.
-  node.exit().remove();
+      // Enter any new nodes.
+      var nodeEnter = node.enter().append("svg:g")
+          .attr("class", "node")
+          .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+          .on("click", click)
+          .on("dblclick", dblclick)
+          .call(force.drag);
 
-  // Re-select for update.
-  link = vis.selectAll("line.link");
-  node = vis.selectAll("g.node");
+      nodeEnter.append("svg:circle")
+          .attr("r", function(d) { return Math.sqrt(d.size) / 10 || 4.5; })
+          .style("fill", color);
 
-  force.on("tick", function() {
-    link.attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+      nodeEnter.append("svg:text")
+          .attr("text-anchor", "middle")
+          .attr("dy", ".35em")
+          .text(function(d) { return d.name; });
 
-    node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-  });
-}
+      // Exit any old nodes.
+      node.exit().remove();
 
-// Color leaf nodes orange, and packages white or blue.
-function color(d) {
-  return d._children ? "#3182bd" : d.children ? "#c6dbef" : "#fd8d3c";
-}
+      // Re-select for update.
+      link = vis.selectAll("line.link");
+      node = vis.selectAll("g.node");
 
-// Toggle children on click.
-function click(d) {
-  if (d.children) {
-    d._children = d.children;
-    d.children = null;
-  } else {
-    d.children = d._children;
-    d._children = null;
-  }
-  update();
-}
+      force.on("tick", function() {
+        link.attr("x1", function(d) { return d.source.x; })
+            .attr("y1", function(d) { return d.source.y; })
+            .attr("x2", function(d) { return d.target.x; })
+            .attr("y2", function(d) { return d.target.y; });
 
-function dblclick(d) {
-	document.getElementById("testSize").innerHTML=d.size;
-}
+        node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+      });
+    }
 
-// Returns a list of all nodes under the root.
-function flatten(root) {
-  var nodes = [], i = 0;
+    // Color leaf nodes orange, and packages white or blue.
+    function color(d) {
+      return d._children ? "#3182bd" : d.children ? "#c6dbef" : "#fd8d3c";
+    }
 
-  function recurse(node) {
-    if (node.children) node.children.forEach(recurse);
-    if (!node.id) node.id = ++i;
-    nodes.push(node);
-  }
+    // Toggle children on click.
+    function click(d) {
+      if (d.children) {
+        d._children = d.children;
+        d.children = null;
+      } else {
+        d.children = d._children;
+        d._children = null;
+      }
+      update();
+    }
 
-  recurse(root);
-  return nodes;
-}
+    function dblclick(d) {
+    	document.getElementById("testSize").innerHTML=d.size;
+    }
+
+    // Returns a list of all nodes under the root.
+    function flatten(root) {
+      var nodes = [], i = 0;
+
+      function recurse(node) {
+        if (node.children) node.children.forEach(recurse);
+        if (!node.id) node.id = ++i;
+        nodes.push(node);
+      }
+
+      recurse(root);
+      return nodes;
+    }
+  }); //document ready
+})();
